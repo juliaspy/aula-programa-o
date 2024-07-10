ad3f5f1cc5001436192533c630f9a86c4f56a7c2
Júlia <valencajulia81@gmail.com>
2024-07-10 12:29:50 -0300

    aula de interpretação

---
+++ teste.js
@@ -0,0 +1,2 @@
+// matheus musico, corda violao partiu, tres lojas, eu estou no canela, avenida 7, piedade, gamboa, mais barata mais longe, quero ir na mais barata. qual iria? 
+let matheus = ["musico", "violao"]
