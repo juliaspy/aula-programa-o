2263c89a4989b705925731ab134fe03e080b2825
Júlia <valencajulia81@gmail.com>
2024-07-17 12:12:45 -0300

    aula de interpretação

---
+++ star.js
@@ -0,0 +1,8 @@
+//calça tem 4 bolsos, 2 primeiros = mais fundos. primeiro bolso = carteira, celular e chave. segundo bolso = cadeado, outra chave e cartão de visita
+let calça = {
+    "bolso1": ["carteira", "celular", "chave"],
+    "bolso2": ["cadeado", "chave", "cartãoVisita"],
+    "bolso3": null,
+    "bolso4": null
+}
+console.log(calça.bolso1 + calça.bolso2 + calça.bolso3 + calça.bolso4)
