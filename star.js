d3b018d3ea219200e5f1d465ba8e99677a806ad9
Júlia <valencajulia81@gmail.com>
2024-07-24 12:22:30 -0300

    aula de interpretação 2

--- star.js
+++ star.js
@@ -1,8 +1,20 @@
 //calça tem 4 bolsos, 2 primeiros = mais fundos. primeiro bolso = carteira, celular e chave. segundo bolso = cadeado, outra chave e cartão de visita
 let calça = {
-    "bolso1": ["carteira", "celular", "chave"],
-    "bolso2": ["cadeado", "chave", "cartãoVisita"],
-    "bolso3": null,
-    "bolso4": null
+    "bolso1": { 
+        "contem": ["carteira", " celular", "chave"],
+        "fundo": true
+    },
+    "bolso2": {
+        "contem":["cadeado", "chave", "cartãoVisita"],
+        "fundo": true
+    },
+    "bolso3": {
+        "contem": [], 
+        "fundo": false
+    },
+    "bolso4": { 
+        "contem": [],
+        "fundo": false
+    }
 }
-console.log(calça.bolso1 + calça.bolso2 + calça.bolso3 + calça.bolso4)
+console.log("Na minha calça tem" + Object.keys(calça) + "no bolso 1, contem" + calça.bolso1.contem)
