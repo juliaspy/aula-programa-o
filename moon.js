c81973caa848bb4b1c56b1aeee65d12b20b8d53e
Júlia <valencajulia81@gmail.com>
2024-08-07 12:30:37 -0300

    aula de for

---
+++ moon.js
@@ -0,0 +1,6 @@
+let nomes = ["marcelo", "julia", "jamile", "thayna", "debora", "claudio", "bruno", "victor", "beatriz", "isaura"]
+for (var i = 0; i < 4; i++) {
+    console.log(i);
+    console.log(nomes[i])
+    // more statements
+  }
