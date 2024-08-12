52522c2a0e2c99891e08092b832313ef2f94850e
Júlia <valencajulia81@gmail.com>
2024-08-12 12:18:36 -0300

    html vapo

---
+++ julia/cond.js
@@ -0,0 +1,26 @@
+//if(condição) {resultado}
+/*
+let numero = 21
+
+if(numero == 21) {console.log("Número é 21")}
+
+console.log(numero)
+
+
+let idade = 17
+
+if(idade == 18) {console.log("entra")} else {console.log("Não entra")}
+
+console.log(idade)
+
+*/
+
+//objeto, array, numero e String
+let julia = ["a", "b", "c"]
+let moon = {
+    "d": "", 
+    "e": "", 
+    "f": ""
+}
+let star = "texto"
+let numero = 1
